7251ce0073ea19c5a8b4ebbb0b5aafe2f5ff6919
Ajay Umakanth <ajayunp@gmail.com>
2024-05-05 11:23:54 +0200

    Inital files

---
+++ DiscriminativeApproach/ConvertToOWL.py
@@ -0,0 +1,151 @@
+from rdflib import Graph, Literal, RDF, RDFS, Namespace, OWL
+from torch_geometric.data import HeteroData
+from rdflib.term import URIRef
+
+class ConvertToOWL():
+    def __init__(self, data: HeteroData, namespace: str, owlGraphPath: str, create_data_properties: bool = True, add_edge_counts: bool = False, create_nominals: bool = False) -> None:
+        """Converts the heterodata into an OWL format
+
+        Args:
+            data (HeteroData): The data the GNN was trained with and which will be used for explanation.
+            namespace (str): The namespace on which the graph will be built.
+            owlGraphPath (str): Where the graph will be stored.
+            create_data_properties (bool): Flag to create data properties in the OWL graph.
+            add_edge_counts (bool): Flag to add edge counts to the OWL graph.
+            create_nominals (bool): Flag to create nominal classes for node features in the OWL graph.
+
+        """
+
+        self.dataset = data
+        self.namespace = namespace
+        self.owlGraphPath = owlGraphPath
+        self.create_nominals = create_nominals
+        self.create_data_properties = create_data_properties
+        self.add_edge_counts = add_edge_counts
+        self.graph = Graph()
+
+    #Builds the OWL graph using the provided heterogenous data.
+    def buildGraph(self):
+        self._createClasses()
+        self._buildObjectProperties()
+        if (self.create_data_properties):
+            self._buildDataProperties()
+        self._buildNodes()
+        self._buildEdges()
+        if (self.add_edge_counts):
+            self._buildEdgeCounts()
+        self.graph.serialize(self.owlGraphPath, format="xml")
+        
+    #Creates OWL classes based on node types in the dataset.
+    def _createClasses(self):
+        classNamespace = Namespace(self.namespace)
+        for node in self.dataset.node_types:
+            self.graph.add((classNamespace[node], RDF.type, OWL.Class))
+            if self.create_nominals:
+                n = 0
+                if "x" in self.dataset[node]:
+                    n = self.dataset[node].x.size(dim=1)
+                if "num_nodes" in self.dataset[node]: 
+                    n = self.dataset[node].num_nodes
+                for i in range(n):
+                    self.graph.add((classNamespace[f'{node}_{i+1}'], RDF.type, OWL.Class))
+                    self.graph.add((classNamespace[f'{node}_{i+1}'], RDFS.subClassOf, classNamespace[node]))
+    
+    #Builds OWL datatype properties (attributes) for each node type in the heterodata.
+    def _buildDataProperties(self):
+        classNamespace = Namespace(self.namespace)
+        for node in self.dataset.node_types:
+            if "x" in self.dataset[node]:
+                tensorValues = self.dataset[node].x
+                n = tensorValues.size(1)
+                for i in range(n):
+                    # Create object property to connect property class to nodes
+                    propertyObjectProperty = classNamespace[f'has_{node}_property_{i+1}']
+                    self.graph.add((propertyObjectProperty, RDF.type, RDF.Property))
+                    self.graph.add((propertyObjectProperty, RDFS.domain, classNamespace[node]))
+                    self.graph.add((propertyObjectProperty, RDFS.range, Literal(True)))
+    
+    #Builds OWL object properties (relationships) based on each edge types in the dataset.
+    def _buildObjectProperties(self):
+        classNamespace = Namespace(self.namespace)
+        for edgeType in self.dataset.edge_types:
+            s, p, o = edgeType
+            self.graph.add((classNamespace[p], RDF.type, OWL.ObjectProperty))
+            self.graph.add((classNamespace[p], RDFS.domain, classNamespace[s]))
+            self.graph.add((classNamespace[p], RDFS.range, classNamespace[o]))
+
+    #Builds individual nodes in the OWL graph.
+    def _buildNodes(self):
+        nodes = self.dataset.node_types
+        classNamespace = Namespace(self.namespace)
+        rdf = RDF
+        for node in nodes:
+            if "x" in self.dataset[node]:
+                tensorValues = self.dataset[node].x
+                for row_idx, person in enumerate(tensorValues):
+                    newNode = classNamespace[f'{node}#{row_idx+1}']
+                    if self.create_nominals:
+                        self.graph.add((newNode, rdf.type, classNamespace[f'{node}_{row_idx+1}']))
+                    else:
+                        self.graph.add((newNode, rdf.type, classNamespace[node]))
+                    self.graph.add((newNode, rdf.type, OWL.NamedIndividual))
+                    if self.create_data_properties:
+                        for col_idx, property in enumerate(person):
+                            val = property.item()
+                            if val == 1:
+                                propertyObjectProperty = classNamespace[f'has_{node}_property_{col_idx+1}']
+                                self.graph.add((newNode, propertyObjectProperty, Literal(True)))
+            if "num_nodes" in self.dataset[node]: 
+                num_nodes = self.dataset[node].num_nodes
+                for idx in range(num_nodes):
+                    newNode = classNamespace[f'{node}#{idx+1}']
+                    self.graph.add((newNode, rdf.type, classNamespace[node]))
+
+    #Builds edges between nodes in the OWL graph.
+    def _buildEdges(self):
+        edgeTypes = self.dataset.edge_types
+        classNamespace = Namespace(self.namespace)
+        for edgeType in edgeTypes:
+            s, p, o = edgeType
+            edges = self.dataset[edgeType].edge_index
+            colSize = edges.size()[1]
+            for col in range(colSize):
+                s_idx = edges[0][col].item()
+                o_idx = edges[1][col].item()
+                self.graph.add((classNamespace[f'{s}#{s_idx+1}'], classNamespace[p], classNamespace[f'{o}#{o_idx+1}']))
+
+    #Builds OWL datatype properties for edge counts.
+    def _buildEdgeCounts(self):
+        classNamespace = Namespace(self.namespace)
+        nodeCounts = {}
+
+        for edge_type in self.dataset.edge_types:
+            s, _, o = edge_type
+            edge_index = self.dataset[edge_type].edge_index
+            nodes, counts = edge_index[0].unique(return_counts=True)
+            for node, count in zip(nodes, counts):
+                if s not in nodeCounts:
+                    nodeCounts[s] = {}
+                if node.item()+1 not in nodeCounts[s]:
+                    nodeCounts[s][node.item()+1] = { "outgoing": 0, "incoming": 0 }
+                nodeCounts[s][node.item()+1]["outgoing"] += count.item()
+            
+            nodes, counts = edge_index[1].unique(return_counts=True) 
+            for node, count in zip(nodes, counts):
+                if o not in nodeCounts:
+                    nodeCounts[o] = {}
+                if node.item()+1 not in nodeCounts[o]:
+                    nodeCounts[o][node.item()+1] = { "outgoing": 0, "incoming": 0 }
+                nodeCounts[o][node.item()+1]["incoming"] += count.item()
+
+        for nodeType in nodeCounts:
+            self.graph.add((classNamespace[f'{nodeType}_incoming'], RDF.type, OWL.DatatypeProperty))
+            self.graph.add((classNamespace[f'{nodeType}_incoming'], RDFS.domain, classNamespace[node]))
+            self.graph.add((classNamespace[f'{nodeType}_incoming'], RDFS.range, XSD.double))
+            self.graph.add((classNamespace[f'{nodeType}_outgoing'], RDF.type, OWL.DatatypeProperty))
+            self.graph.add((classNamespace[f'{nodeType}_outgoing'], RDFS.domain, classNamespace[node]))
+            self.graph.add((classNamespace[f'{nodeType}_outgoing'], RDFS.range, XSD.double))
+            for node in nodeCounts[nodeType]:
+                newNode = classNamespace[f'{nodeType}#{node}']
+                self.graph.add((newNode, classNamespace[f'{nodeType}_incoming'], Literal(nodeCounts[nodeType][node]["incoming"])))
+                self.graph.add((newNode, classNamespace[f'{nodeType}_outgoing'], Literal(nodeCounts[nodeType][node]["outgoing"])))
---
+++ DiscriminativeApproach/DiscriminativeExplainer.py
@@ -0,0 +1,183 @@
+"""Methods and classes that inherit the parent explainer class"""
+from XGNN.Infrastructure.GNN import GNN
+from XGNN.Infrastructure.Explainer import Explainer
+from torch_geometric.data import HeteroData
+from ontolearn.owlapy.model import OWLClassExpression
+from XGNN.DiscriminativeApproach.ConvertToOWL import ConvertToOWL
+from ontolearn.knowledge_base import KnowledgeBase
+from ontolearn.concept_learner import EvoLearner
+from ontolearn.learning_problem import PosNegLPStandard
+from ontolearn.owlapy.model import OWLNamedIndividual, IRI
+from XGNN.Infrastructure.Utils import _find_classes_with_y_labels
+from ontolearn.metrics import Accuracy, F1
+from ontolearn.abstracts import AbstractScorer
+from typing import Optional
+import os
+from ontolearn.owlapy.model import OWLNamedIndividual, IRI, OWLObjectIntersectionOf, \
+    OWLClassExpression, OWLObjectUnionOf, OWLObjectComplementOf, OWLObjectOneOf, \
+    OWLObjectMaxCardinality, OWLObjectMinCardinality, OWLObjectAllValuesFrom, \
+    OWLObjectSomeValuesFrom, OWLClass, OWLNothing
+import random
+import re
+
+class DiscriminativeExplainer(Explainer):
+    """ An abstract class which represent an explainer. An explainer should be able to use a label to generate a
+    model-level explanation based on a given GNN and the related dataset.
+
+    Attributes:
+        gnn: The GNN the class is able to explain.
+        data: The dataset the GNN is based on.
+    """
+    def __replace_with_nominal(self, ce: OWLClassExpression): #, top_nodes_dict: dict[list[int], str]):
+        if isinstance(ce, OWLObjectIntersectionOf):
+            ops = list()
+            for op in ce.operands():
+                mutated = self.__replace_with_nominal(op)
+                ops.append(mutated)
+            return OWLObjectIntersectionOf(ops)
+        if isinstance(ce, OWLObjectUnionOf):
+            ops = list()
+            for op in ce.operands():
+                mutated = self.__replace_with_nominal(op)
+                ops.append(mutated)
+            return OWLObjectUnionOf(ops)
+        if isinstance(ce, OWLObjectComplementOf):
+            return OWLObjectComplementOf(self.__replace_with_nominal(ce.get_operand()))
+        if isinstance(ce, OWLClass):
+            className = ce.get_iri().get_remainder()
+            isNominal = re.findall(r'_\d+$', className)
+            if not isNominal:
+                return ce
+            else:
+                splits = className.rsplit("_", 1)
+                individual = OWLNamedIndividual(IRI("/", splits[0]+'#'+splits[1]))
+                return OWLObjectOneOf(individual)
+        if isinstance(ce, OWLObjectSomeValuesFrom):
+            ce2 = self.__replace_with_nominal(ce.get_filler())
+            return OWLObjectSomeValuesFrom(ce.get_property(), ce2)
+        if isinstance(ce, OWLObjectAllValuesFrom):
+            ce2 = self.__replace_with_nominal(ce.get_filler())
+            return OWLObjectAllValuesFrom(ce.get_property(), ce2)
+        if isinstance(ce, OWLObjectMaxCardinality):
+            ce2 = self.__replace_with_nominal(ce.get_filler())
+            return OWLObjectMaxCardinality(ce.get_cardinality(), ce.get_property(), ce2)
+        if isinstance(ce, OWLObjectMinCardinality):
+            ce2 = self.__replace_with_nominal(ce.get_filler())
+            return OWLObjectMinCardinality(ce.get_cardinality(), ce.get_property(), ce2)
+        return ce
+
+    def __init__(self, gnn: GNN, data: HeteroData, namespace = "http://example.org/", owl_graph_path = "./owlGraphs/example.owl", generate_new_owl_file: bool = False, create_nominals: bool = False,  add_edge_counts: bool = False) -> None:
+        """Initializes the explainer based on the given GNN and the Dataset. After the initialization the object should
+        be able to produce explanations of single labels.
+
+        Args:
+            gnn: The GNN the explainer should be able to explain.
+            data: The data the GNN was trained with and which will be used for explanation.
+            namespace: The namespace to which we have to assign the nodes of a graph to.
+            create_nominals: Create seperate classes for each individual to support nominals
+        """
+        self.gnn = gnn
+        self.data = data
+        self.namespace = namespace
+        self.owl_graph_path = owl_graph_path
+        self.create_nominals = create_nominals
+        self.classNames = _find_classes_with_y_labels(self.data, first_only=False)
+        if generate_new_owl_file and os.path.isfile(self.owl_graph_path):
+            os.remove(self.owl_graph_path)
+        if not os.path.isfile(self.owl_graph_path):
+            self.owlGraph = ConvertToOWL(data=self.data, namespace=self.namespace, owlGraphPath=self.owl_graph_path, create_nominals=create_nominals, add_edge_counts=add_edge_counts)
+            self.owlGraph.buildGraph()
+        self.knowledge_base = KnowledgeBase(path=self.owl_graph_path)
+
+    def explain(self, 
+            label: int,
+            n: Optional[int] = 5,
+            use_data_properties: Optional[bool] = True,
+            debug: Optional[bool] = False,
+            max_runtime: Optional[int] = 60,
+            num_generations: Optional[int] = 600,
+            quality_func: Optional[AbstractScorer] = None) -> OWLClassExpression:
+        """Explains based on the GNN a given label. The explanation is in the form of a Class Expression.
+
+        Args:
+            label: The label which should be explained.
+            n: the maximum number of hypotheses that will be produced
+            debug: use the y label in the node of the heterodata object
+            use_data_properties: Use the data properties in the explainer
+            max_runtime (int): max_runtime: Limit to stop the algorithm after n seconds
+            num_generations (int): Number of generation for the evolutionary algorithm
+
+        Returns:
+            A class expression which is an explanation of the given label based on the GNN and the dataset.
+        """
+
+        if quality_func is None:
+            quality_func = F1()
+        self.model = EvoLearner(knowledge_base=self.knowledge_base, use_data_properties=use_data_properties, max_runtime=max_runtime, num_generations=num_generations, quality_func=quality_func)
+        positive_examples = []
+        negative_examples = []
+        predictions = {}
+        if (not debug):
+            predictions = self.gnn.predict_all(new_data=self.data)
+        for node_type in self.data.node_types:
+            if (debug):
+                if node_type in self.classNames:
+                    yLabels = self.data[node_type].y
+                    for idx, yLabel in enumerate(yLabels):
+                        node = self.namespace + node_type + "#" + str(idx+1)
+                        isPositive = yLabel.item() == label
+                        if (isPositive):
+                            positive_examples.append(node)
+                        else:
+                            negative_examples.append(node)
+                else:
+                    noOfNodes = self.data[node_type].x.size()[0]
+                    for idx in range(noOfNodes):
+                        node = self.namespace + node_type + "#" + str(idx+1)
+                        negative_examples.append(node)
+            else:
+                if node_type in predictions:
+                    nodeTypePredictions = predictions[node_type]
+                    for idx, nodePrediction in enumerate(nodeTypePredictions):
+                        node = self.namespace + node_type + "#" + str(idx+1)
+                        isPositive = nodePrediction.item() == label
+                        if (isPositive):
+                            positive_examples.append(node)
+                        else:
+                            negative_examples.append(node)
+                else:
+                    noOfNodes = self.data[node_type].x.size()[0]
+                    for idx in range(noOfNodes):
+                        node = self.namespace + node_type + "#" + str(idx+1)
+                        negative_examples.append(node)
+        
+        if len(positive_examples) == 0:
+            return [OWLNothing]
+
+        typed_pos = set(map(OWLNamedIndividual, map(IRI.create, set(positive_examples))))
+        typed_neg = set(map(OWLNamedIndividual, map(IRI.create, set(negative_examples))))
+        lp = PosNegLPStandard(pos=typed_pos, neg=typed_neg)
+        
+        # cross check if the quality stated is within the threshold of the actual quality
+        while True:
+            self.model.fit(lp)
+            hypotheses = list(self.model.best_hypotheses(n=n))
+            accepted_hypotheses = []
+            for hypothesis in hypotheses:
+                evaluated_concept = self.model.kb.evaluate_concept(hypothesis.concept, self.model.quality_func, self.model._learning_problem)
+                thresh = 0.05
+                accepted_hypotheses.append(hypothesis.quality*(1+thresh) > evaluated_concept.q or hypothesis.quality*(1-thresh) < evaluated_concept.q)
+
+            hypotheses = [element for element, keep in zip(hypotheses, accepted_hypotheses) if keep]
+
+            if len(hypotheses) > 0:
+                break
+        return hypotheses, self.model
+
+        [print(_) for _ in hypotheses]
+        if self.create_nominals:
+            hypotheses = [self.__replace_with_nominal(hypothesis.concept) for hypothesis in hypotheses]
+        else:
+            hypotheses = [hypothesis.concept for hypothesis in hypotheses]
+
+        return hypotheses
